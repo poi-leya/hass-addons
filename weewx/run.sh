484ed97c47da61c43c795c26f489fd8807ea6847
Yannick Schubert <yannick.schubert@porscheinformatik.com>
2025-02-20 12:26:31 +0100

    init

--- weewx/run.sh
+++ weewx/run.sh
@@ -2,6 +2,7 @@
 
 CONFIG_PATH=/data/options.json
 PAIR="$(jq --raw-output '.pair' $CONFIG_PATH)"
+CONFIGURE="$(jq --raw-output '.configure' $CONFIG_PATH)"
 DRIVER="$(jq --raw-output '.driver' $CONFIG_PATH)"
 LATITUDE="$(jq --raw-output '.latitude' $CONFIG_PATH)"
 LONGITUDE="$(jq --raw-output '.longitude' $CONFIG_PATH)"
@@ -10,12 +11,18 @@ ALTITUDEUNIT="$(jq --raw-output '.altitudeUnit' $CONFIG_PATH)"
 LOCATION="$(jq --raw-output '.location' $CONFIG_PATH)"
 UNITS="$(jq --raw-output '.units' $CONFIG_PATH)"
 
-weectl station reconfigure --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=/config/weewx.conf
+if [ "$CONFIGURE" == 'true' ]
+then
+  weectl station reconfigure --driver=$DRIVER --latitude=$LATITUDE --longitude=$LONGITUDE --altitude=$ALTITUDE,$ALTITUDEUNIT --location=$LOCATION --units=$UNITS --no-prompt --config=/config/weewx.conf
+fi
 
 if [ "$PAIR" == 'true' ]
 then
   weectl device --pair
 fi
 
-weewxd /config/weewx.conf
+if [ "$CONFIGURE" == 'false' ]
+then
+  weewxd /config/weewx.conf
+fi
 #sleep infinity
